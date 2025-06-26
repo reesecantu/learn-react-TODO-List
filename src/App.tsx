400368a35f5e87744ef44f81ca2b61b3cee2867c
reesecantu <65140498+reesecantu@users.noreply.github.com>
2025-06-26 18:07:43 -0400

    Create list of todo items with dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import TodoListRow from "./components/TodoListRow";
+import { dummyData } from "./data/todos";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  function setTodoCompleted(id: number, completed: boolean) {
+    alert(`Todo with id ${id} is now ${completed ? "completed" : "not completed"}`)
+  }
+ 
+ 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="py-10 h-screen space-y-5">
+      <h1 className="font-bold text-3xl text-center"> Your Todos </h1>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
+        <div className="space-y-2">
+          {dummyData.map((todo) => (
+            <TodoListRow 
+            key ={todo.id}
+            todo={todo}
+            onCompletedChange={setTodoCompleted}/>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/data/todos.ts
@@ -0,0 +1,20 @@
+import type { Todo } from "../types/todo";
+
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "learn react",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "get a cool job",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "travel to Italy",
+        completed: true,
+    },
+];
