1cf558cb71b87b9cbb94dac00bdaeb2a1c271e13
reesecantu <65140498+reesecantu@users.noreply.github.com>
2025-06-26 18:16:19 -0400

    dynamically update todo item state

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
+import { useState } from "react";
 import TodoListRow from "./components/TodoListRow";
 import { dummyData } from "./data/todos";
 
 function App() {
+  const [todos, setTodos] = useState(dummyData);
+
   function setTodoCompleted(id: number, completed: boolean) {
-    alert(`Todo with id ${id} is now ${completed ? "completed" : "not completed"}`)
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => (todo.id === id ? { ...todo, completed } : todo))
+    );
   }
- 
- 
+
   return (
     <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center"> Your Todos </h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
         <div className="space-y-2">
-          {dummyData.map((todo) => (
-            <TodoListRow 
-            key ={todo.id}
-            todo={todo}
-            onCompletedChange={setTodoCompleted}/>
+          {todos.map((todo) => (
+            <TodoListRow
+              key={todo.id}
+              todo={todo}
+              onCompletedChange={setTodoCompleted}
+            />
           ))}
         </div>
       </div>
--- src/data/todos.ts
+++ src/data/todos.ts
@@ -15,6 +15,6 @@ export const dummyData: Todo[] = [
     {
         id: 3,
         title: "travel to Italy",
-        completed: true,
+        completed: false,
     },
 ];
